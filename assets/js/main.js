cfa6345eda936e6de8f822524350d6675f8ad6ff
mahiarirani <mahiarirani@users.noreply.github.com>
2023-10-21 07:25:31 +0000

    deploy: 4ee9df51ad6bc630831983c466da1f596b0f5834

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', function () {
-    if (document.getElementsByClassName('keep-scroll').length)
+    if (document.getElementsByClassName('keep-scroll').length) {
         localStorage.top = window.scrollY;
+    }
 })
 window.addEventListener('load', function () {
-    if (document.getElementsByClassName('keep-scroll').length)
+    if (document.getElementsByClassName('keep-scroll').length) {
         window.scrollTo({top: localStorage.top || 0, behavior: 'smooth'});
+    }
 })
