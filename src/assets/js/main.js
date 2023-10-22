7df53f201527fa571935da0ea9b60a76f466d388
Mahyar Iranibazaz <mahiarirani@protonmail.com>
2023-10-23 04:04:21 +0800

    update: use sessionStorage instead of localStorage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', function () {
     if (document.getElementsByClassName('keep-scroll').length) {
-        localStorage.top = window.scrollY;
+        sessionStorage.top = window.scrollY;
     }
 })
 window.addEventListener('load', function () {
     if (document.getElementsByClassName('keep-scroll').length) {
-        window.scrollTo({top: localStorage.top || 0, behavior: 'smooth'});
+        window.scrollTo({top: sessionStorage.top || 0, behavior: 'smooth'});
     }
 })
